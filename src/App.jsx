c6c6281d64f9a195889ce02acff6d986967fbc0d
takoyaki1BANYA <takoyaki5656umaide@gmail.com>
2026-07-21 22:56:18 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Oracle App</h1>
+      <p>正常に動作しています。</p>
+    </div>
+  );
+}
